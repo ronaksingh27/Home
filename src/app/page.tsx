c0b12e582fdf3294f99a0c778d0cdc5a0d0e274e
ronak <ronaksingh2708@gmail.com>
2024-12-18 18:30:03 +0530

    bg pattern added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Card from "./Card/Card";
 export default function Home() {
   return (
 
-    <div className="flex justify-center items-center h-screen w-screen bg-gray-200" >
+    <div className="flex justify-center items-center h-screen w-screen bg-gray-200"
+    style={{
+      backgroundImage: "url('/images/bg.jpg')", // Path to your image
+      backgroundRepeat: 'repeat', // Repeats the image
+      backgroundSize: '500px 500px' // Adjusts the size of the image to its original size
+  }} >
       
       <Card/>
 
